f4bf8dbc1daca567fc4807818796d55f2377afc5
Xavier Alcerro <xavieralcerro@gmail.com>
2026-02-14 23:53:18 -0600

    Commit 2: Agregar información académica

--- EjercicioGitX/Program.cs
+++ EjercicioGitX/Program.cs
@@ -10,6 +10,11 @@ namespace EjercicioGitX
             Console.WriteLine("Nombre: Xavier Andres Alcerro Alcerro");
             Console.WriteLine("Cuenta: 20221930056");
             Console.WriteLine("Fecha: " + DateTime.Now.ToString("dd/MM/yyyy"));
+
+            Console.WriteLine("\n=== INFORMACIÓN ACADÉMICA ===");
+            Console.WriteLine("Carrera: Ingeniería en Sistemas");
+            Console.WriteLine("Semestre: Actual");
+            Console.WriteLine("Asignatura: Programación");
         }
     }
 }
