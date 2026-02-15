7bc566ae99de535ac556672410773cc5117efb2c
Xavier Alcerro <xavieralcerro@gmail.com>
2026-02-14 23:51:03 -0600

    Commit 1: Agregar datos personales

--- EjercicioGitX/Program.cs
+++ EjercicioGitX/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace EjercicioGitX
+﻿using System;
+
+namespace EjercicioGitX
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("=== MIS DATOS PERSONALES ===");
+            Console.WriteLine("Nombre: Xavier Andres Alcerro Alcerro");
+            Console.WriteLine("Cuenta: 20221930056");
+            Console.WriteLine("Fecha: " + DateTime.Now.ToString("dd/MM/yyyy"));
         }
     }
 }
